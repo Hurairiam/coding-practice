20b49265c454707b28b009e332664f9f8e47c408
Hurairiam <hurairiam@gmail.com>
2025-07-19 13:05:17 +0600

    Auto commit: update files

--- C Problems/4. Looping in C – Repetition & Control Structures/12. GCD and LCM.c
+++ C Problems/4. Looping in C – Repetition & Control Structures/12. GCD and LCM.c
@@ -1,2 +1,20 @@
 //Write a program to find GCD (greatest common divisor or HCF) and LCM (least common multiple) of two numbers.
 
+#include<stdio.h>
+int main()
+{   int a, b, gcd, lcm, min, i;
+    printf("Enter two numbers : ");
+    scanf("%d %d", &a, &b);
+    if(a>b){min=b;}
+       else{min=a;};
+
+    for(i=1; i<=min ; i++)
+    {
+        if(a%i==0&&b%i==0)
+        {gcd=i;}
+    }
+    lcm=(a*b)/gcd;
+   printf("GCD = %d, LCM = %d", gcd, lcm);
+
+   return 0;
+}
