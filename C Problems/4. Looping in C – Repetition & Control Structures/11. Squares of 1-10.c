fe9521be60733407dcdd3df88823bff57e0f8748
Hurairiam <hurairiam@gmail.com>
2025-07-19 12:40:58 +0600

    Auto commit: update files

---
+++ C Problems/4. Looping in C – Repetition & Control Structures/11. Squares of 1-10.c
@@ -0,0 +1,10 @@
+//Write a program to print the numbers from 1 to 10 and their squares using for Loop
+
+#include <stdio.h>
+
+int main()
+{
+    for(int i=1; i<=10; i++)
+        {printf("%d, %d\n", i, i*i);}
+    return 0;
+}
