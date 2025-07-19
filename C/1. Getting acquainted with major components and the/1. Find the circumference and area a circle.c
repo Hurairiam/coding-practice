cbbcd9e564fd0599cc01c618b64a7381a91de265
Hurairiam <hurairiam@gmail.com>
2025-07-19 09:31:43 +0600

    Auto commit: update files

---
+++ C/1. Getting acquainted with major components and the/1. Find the circumference and area a circle.c
@@ -0,0 +1,17 @@
+/*
+1. Write a C program to enter the radius of a circle and find its circumference and area. Note that
+Circumference = 2 x π x radius, Area = π x (radius)2, and assume π=3.1416
+Input : 2
+Output 12.560
+*/
+#include <stdio.h>
+
+int main()
+{
+    float rad;
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &rad);
+    printf("Circumference: %.2f\n", 2*3.1416*rad);
+    printf("Area: %.2f\n", 3.1416*rad*rad);
+    return 0;
+}
