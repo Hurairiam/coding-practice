a82d6b4ef5c11c87a1e9456f9d0372e3c94be748
Hurairiam <hurairiam@gmail.com>
2025-07-26 06:44:05 +0600

    Auto commit: update files

--- C Problems/7. 1D Array/5. Split Even and Odd Elements into Separate Arrays.c
+++ C Problems/7. 1D Array/5. Split Even and Odd Elements into Separate Arrays.c
@@ -1,43 +1,41 @@
-//Write a program to split even and odd elements of an array into two arrays
 
-#include <stdio.h>
+#include<stdio.h>
 
-int main() 
+int main()
 {
-    int arr[100], even[100], odd[100];
+    int og[100], even[100], odd[100];
     int i, n;
-    int evenCount = 0, oddCount = 0;
+    int evenCount=0, oddCount=0;
 
-    printf("Enter number of elements: ");
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    for(i = 0; i < n; i++) {
+    for(i=0; i<n; i++)
+    {
         printf("Element %d: ", i+1);
-        scanf("%d", &arr[i]);
+        scanf("%d", &og[i]);
 
 
-        if(arr[i] % 2 == 0) 
+        if(og[i]%2==0)
        {
-            even[evenCount] = arr[i];
+            even[evenCount]=og[i];
             evenCount++;
        }
-     else 
+     else
        {
-            odd[oddCount] = arr[i];
+            odd[oddCount]=og[i];
             oddCount++;
        }
     }
 
 
     printf("\nEven elements: ");
-    for(i = 0; i < evenCount; i++) {
-        printf("%d ", even[i]);
-    }
+    for(i=0; i<evenCount; i++)
+    {printf("%d ", even[i]);}
 
     printf("\nOdd elements: ");
-    for(i = 0; i < oddCount; i++) {
-        printf("%d ", odd[i]);
-    }
+    for(i=0; i<oddCount; i++)
+    {printf("%d ", odd[i]);}
 
     return 0;
 }
