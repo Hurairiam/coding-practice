02cdcd85e480a4fc76ecbf4d7272659b2a4d7353
Hurairiam <hurairiam@gmail.com>
2025-08-11 11:18:28 +0600

    Auto commit: update files

--- C Problems/Finals/Strings  1. Concatenate two strings (longer uppercase at the end) (better).c
+++ C Problems/Finals/Strings  1. Concatenate two strings (longer uppercase at the end) (better).c
@@ -5,66 +5,31 @@
 #include <stdio.h>
 
 int main() {
-    char s1[100], s2[100], result[200];
-    char *p;
+    char s1[100], s2[100], *shorter, *longer;
     int len1 = 0, len2 = 0;
+    char *p;
 
-    printf("Enter first string: ");
     gets(s1);
-
-    printf("Enter second string: ");
     gets(s2);
 
-    // Count length of s1 using pointer
-    p = s1;
-    while (*p != '\0') {
-        len1++;
-        p++;
-    }
+    // Calculate lengths
+    for (p = s1; *p != '\0'; p++) len1++;
+    for (p = s2; *p != '\0'; p++) len2++;
 
-    // Count length of s2 using pointer
-    p = s2;
-    while (*p != '\0') {
-        len2++;
-        p++;
-    }
-
-    char *shorter, *longer;
-
+    // Decide shorter and longer
     if (len1 <= len2) {
-        shorter = s1;
-        longer = s2;
+        shorter = s1; longer = s2;
     } else {
-        shorter = s2;
-        longer = s1;
+        shorter = s2; longer = s1;
     }
 
     // Convert longer string to uppercase in place
-    p = longer;
-    while (*p != '\0') {
-        if (*p >= 'a' && *p <= 'z') {
-            *p = *p - 32;
-        }
-        p++;
+    for (p = longer; *p != '\0'; p++) {
+        if (*p >= 'a' && *p <= 'z') *p = *p - 32;
     }
 
-    // Copy shorter string to result
-    int i = 0;
-    while (shorter[i] != '\0') {
-        result[i] = shorter[i];
-        i++;
-    }
-
-    // Append uppercase longer string to result
-    int j = 0;
-    while (longer[j] != '\0') {
-        result[i] = longer[j];
-        i++;
-        j++;
-    }
-    result[i] = '\0';
-
-    printf("Concatenated string: %s\n", result);
+    // Print shorter then uppercase longer together
+    printf("%s%s\n", shorter, longer);
 
     return 0;
 }
