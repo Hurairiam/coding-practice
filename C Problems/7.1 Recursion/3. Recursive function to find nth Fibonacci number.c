0b66a9a7f5844f088fbf3ac21a4a93544f18c002
Hurairiam <hurairiam@gmail.com>
2025-08-19 08:27:36 +0600

    Auto commit: update files

--- C Problems/7.1 Recursion/3. Recursive function to find nth Fibonacci number.c
+++ C Problems/7.1 Recursion/3. Recursive function to find nth Fibonacci number.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 
-// Function to search element
-int linearSearch(int arr[], int n, int key) {
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == key) {
-            return i; // return index
-        }
-    }
-    return -1; // not found
+// Recursive function to find nth Fibonacci number
+int fibonacci(int n) {
+    if (n == 0) return 0;  // base case
+    if (n == 1) return 1;  // base case
+    return fibonacci(n - 1) + fibonacci(n - 2); // recursive call
 }
 
 int main() {
-    int arr[] = {10, 25, 30, 45, 50};
-    int n = 5, key = 30;
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
 
-    int index = linearSearch(arr, n, key);
-
-    if (index != -1)
-        printf("Element %d found at index %d\n", key, index);
-    else
-        printf("Element %d not found\n", key);
+    printf("The %dth Fibonacci number is: %d\n", n, fibonacci(n));
 
     return 0;
 }
