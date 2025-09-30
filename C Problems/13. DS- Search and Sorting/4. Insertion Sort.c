94bebf16dcec8531d5c147e2e06adfb82b03d5ef
Hurairiam <hurairiam@gmail.com>
2025-09-30 08:00:34 +0600

    Auto commit: update files

---
+++ C Problems/13. DS- Search and Sorting/4. Insertion Sort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, array[1000], c, d, t;
+
+    printf("Enter number of elements\n");
+    scanf("%d", &n);
+
+    printf("Enter %d integers\n", n);
+    for (c = 0; c < n; c++)
+        scanf("%d", &array[c]);
+
+    // Insertion sort
+    for (c = 1; c < n; c++) {
+        t = array[c];
+        d = c - 1;
+
+        while (d >= 0 && array[d] > t) {
+            array[d + 1] = array[d];
+            d = d - 1;
+        }
+        array[d + 1] = t;
+    }
+
+    printf("Sorted list in ascending order:\n");
+    for (c = 0; c < n; c++)
+        printf("%d\n", array[c]);
+
+    return 0;
+}
